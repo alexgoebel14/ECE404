b07402395d3da73177b143ede339f11854e7cc1e
Alex Goebel <alexgoebel14@gmail.com>
2021-04-13 23:55:28 -0500

    Behind on commits, here is the most up-to-date files

--- HW8/tcpAttackTestScript.py
+++ HW8/tcpAttackTestScript.py
@@ -8,10 +8,10 @@ Created on Tue Mar 30 10:21:03 2021
 from TcpAttack import *
 
 spoofIP = '192.168.4.124'
-targetIP = '128.46.4.92'
-rangeStart = 0
-rangeEnd = 200
-port = 22
+targetIP = '127.0.0.1'
+rangeStart = 9650
+rangeEnd = 9750
+port = 9700
 Tcp = TcpAttack(spoofIP, targetIP)
 Tcp.scanTarget(rangeStart, rangeEnd)
 if Tcp.attackTarget(port,10):
